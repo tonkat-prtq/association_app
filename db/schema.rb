386dbd2e272686ffc314c3e21e6367bf5ba3dcbf
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-10-06 13:24:59 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_042230) do
+ActiveRecord::Schema.define(version: 2019_10_06_042356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_10_06_042230) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +48,5 @@ ActiveRecord::Schema.define(version: 2019_10_06_042230) do
   end
 
   add_foreign_key "blogs", "users"
+  add_foreign_key "books", "authors"
 end
